f7e3e0e27fb0fc6109b6a17141aacf6a21bf41a0
John <34749334+JBukowy@users.noreply.github.com>
2023-03-14 15:51:07 -0500

    end of w2d1

---
+++ src/week1/QuizDriver.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class QuizDriver {
+
+    public static void main(String[] args) {
+
+        //String result = "One Hundred" – "One Hundred and Four";
+
+
+
+        //System.out.println(result);
+
+
+    }
+
+
+
+}
--- src/week1/Shape.java
+++ src/week1/Shape.java
@@ -2,10 +2,19 @@ package week1;
 
 public interface Shape {
 
+    public final static double PI = 3.14;
+
+
     public double area();
 
     public double perimeter();
 
     String getString();
+
+    public static int maximumArea(Shape one, Shape two){
+
+    }
+
+
 
 }
