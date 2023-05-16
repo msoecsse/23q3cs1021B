f46aa22d393cf5f6fb1266b9c5b4963d15ddf77c
John <34749334+JBukowy@users.noreply.github.com>
2023-05-16 15:50:35 -0500

    end of w10d1

---
+++ src/week10/streams/ComparatorExample.java
@@ -0,0 +1,21 @@
+package week10.streams;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ComparatorExample {
+
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog");
+
+        List<String> newList = list.stream().filter(s -> s.length() == 3).filter(s -> s.charAt(0) == 'f').toList();
+
+        System.out.println(newList);
+
+
+    }
+
+}
--- src/week9/functional/ComparatorExample.java
+++ src/week9/functional/ComparatorExample.java
@@ -1,24 +1,35 @@
 package week9.functional;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class ComparatorExample {
 
     public static void main(String[] args) {
 
-        Comparator<Integer> c = (i,j) ->{
-            if(i < j){
-                return 1;
-            }else if(i > j){
+        List<String> list = Arrays.asList("The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog");
+
+        Comparator<String> c = (i,j) ->{
+            if(i.charAt(0) < j.charAt(0)){
                 return -1;
+            }else if(i.charAt(0) > j.charAt(0)){
+                return 1;
             }else{
                 return 0;
             }
         };
 
-        System.out.println(c.compare(4,4));
+        //System.out.println(c.compare("do","ray"));
 
+        System.out.println(list);
 
+        Collections.sort(list, (i,j) ->{
+            return Math.random() > 0.5 ? 1 : -1;
+        });
+
+        System.out.println(list);
     }
 
 }
